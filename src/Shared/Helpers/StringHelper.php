e680a2ea81bd3c0b2c2da1e16bfc5148c15218c5
Kilderson Sena <dersonsena@gmail.com>
2021-01-02 01:13:01 -0300

    chore: creating string helper class

---
+++ src/Shared/Helpers/StringHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Helpers;
+
+final class StringHelper
+{
+    public static function slugify(string $text): string
+    {
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = trim($text, '-');
+        $text = preg_replace('~-+~', '-', $text);
+        $text = strtolower($text);
+
+        return $text;
+    }
+}
