6251ecbab63a82405cf0f00240e2bdb066f857f9
Kilderson Sena <dersonsena@gmail.com>
2021-01-09 23:46:12 -0300

    chore: adding app debug flag

--- public/index.php
+++ public/index.php
@@ -12,7 +12,8 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-defined('APP_ENV') or define('APP_ENV', getenv('APP_ENV'));
+defined('APP_ENV') or define('APP_ENV', $_ENV['APP_ENV']);
+defined('APP_DEBUG_ENABLED') or define('APP_DEBUG_ENABLED', (bool)$_ENV['APP_DEBUG_ENABLED']);
 defined('CACHE_ENABLE') or define('CACHE_ENABLE', isset($_GET['cache']) ? (bool)$_GET['cache'] : true);
 
 require_once __DIR__  . '/../config/di.php';
@@ -20,6 +21,6 @@ require_once __DIR__  . '/../config/di.php';
 $app = AppFactory::createFromContainer($container);
 
 require __DIR__ . '/../config/errors.php';
-require __DIR__ . '/../routes/routes.php';
+require __DIR__ . '/../config/routes.php';
 
 $app->run();
